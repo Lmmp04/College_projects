1343078087a914f994b86a0577032d5627a43f2a
Leo Platti <78324368+Lmmp04@users.noreply.github.com>
2024-02-01 10:26:49 -0500

    Create Area_Calculator.py

---
+++ Area_Calculator.py
@@ -0,0 +1,34 @@
+#This script was written on 1.30.24 by Lmmp04 for a practical programming class. It features a calculator for the area of a circle, rectangle, or a triangle!
+print(".           \           L          PPPPPP     /")
+print(r".           \\          L          P    P   //")
+print(". ===========>>         L          PPPPPP   <<===========")
+print(r".           //          L          P         \\")
+print(".          /            LLLLLLL    P           \ ")
+
+
+def area_rect():
+    print("Area of a Rectangle")
+    width = int(input("Enter the width: "))
+    height = input("Enter the height: ")
+    print ("The Area of the Rectangle is: ", int(width*height))
+    
+
+
+area_rect()
+
+def area_circle():
+    print("The Area of a Circle")
+    radius = int(input("Enter the radius: "))
+    eq = 3.14*radius**2
+    print ("The Area of the Circle is: ", eq)
+
+area_circle()
+
+def area_tri():
+    print("Area of a Triangle: ")
+    base = int(input("Enter the base: "))
+    height = int(input("Enter the Height: "))
+    eq = (.5*base*height)
+    print ("The Area of the Triangle is: ", eq)
+
+area_tri()
